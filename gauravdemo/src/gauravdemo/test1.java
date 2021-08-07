cccf594c92064626240a1da4b4b880b722ddf789
gaurav kumar <gaurav.mca12@gmail,com>
2021-08-07 09:19:18 +0530

    first commit by gaurav

---
+++ gauravdemo/src/gauravdemo/test1.java
@@ -0,0 +1,17 @@
+package gauravdemo;
+import java.io.*;
+public class test1 {
+	
+	
+	public static void main(String args[])
+	{
+		
+		System.out.println("hi i m in test1 class");
+		test2 ob= new test2();
+		ob.test2method();
+		
+		
+	}
+	
+
+}
---
+++ gauravdemo/src/gauravdemo/test2.java
@@ -0,0 +1,11 @@
+package gauravdemo;
+
+public class test2 {
+
+	public void test2method()
+	{
+	
+	System.out.println("hi i m in test2 class");
+	}
+
+}
